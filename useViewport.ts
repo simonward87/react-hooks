756caefed8fd933bbead16ecc40ba0c18c3df3b9
Simon Ward <39803787+simonward87@users.noreply.github.com>
2022-01-05 10:55:14 +0000

    Small code refactoring

--- useViewport.ts
+++ useViewport.ts
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 // useViewport provides the current viewport height and width, which can then
 // be within components for conditional rendering etc.
 
 interface Dimensions {
-  //dimensions in pixels
-  height: number;
-  width: number;
+  // in pixels
+  height: number
+  width: number
 }
 
 export default function useViewport() {
   // Partial is used as object wont be populated until the component mounts
-  const [viewport, setViewport] = useState<Partial<Dimensions>>({});
+  const [viewport, setViewport] = useState<Partial<Dimensions>>({})
 
   useEffect(() => {
     // isolated function can be easily used to both add / remove the event
@@ -19,22 +19,22 @@ export default function useViewport() {
     function handleWindowResize() {
       setViewport({
         height: window.innerHeight,
-        width: window.innerWidth
-      });
+        width: window.innerWidth,
+      })
     }
 
     // initialize the state when the component is first mounted
     if (Object.keys(viewport).length < 2) {
-      handleWindowResize();
+      handleWindowResize()
     }
 
     // add event listener when component mounts
-    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener('resize', handleWindowResize)
 
     // clean up event listener when component unmounts
-    return () => window.removeEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize)
   })
 
   // array used so elements can be destructured and renamed as required
-  return [viewport.height, viewport.width];
+  return [viewport.height, viewport.width]
 }
